90d212115afcea91b04a68ad88f92717f15dadcb
Jan Ryser <jan.ryser@oit.edu>
2024-06-04 15:01:22 -0700

    Margins :)

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -18,7 +18,6 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 using Windows.UI.Xaml.Shapes;
-
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Final
